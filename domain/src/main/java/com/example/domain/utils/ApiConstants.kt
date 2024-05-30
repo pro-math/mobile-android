c0843d51163d82162411c74766037cffa30ccc93
qwertyAL <sergeyiksanov25@gmail.com>
2024-05-30 15:49:13 +0300

    add package utils in data module, add ApiConstants to utils

---
+++ domain/src/main/java/com/example/domain/utils/ApiConstants.kt
@@ -0,0 +1,10 @@
+package com.example.domain.utils
+
+object ApiConstants {
+
+    private const val BASE_URL = ""
+    const val LOGIN_URL = "$BASE_URL/auth/login/"
+    const val GAME_SESSIONS_URL = "$BASE_URL/game_sessions"
+    const val USERS_URL = "$BASE_URL/users"
+
+}
