1a0115a50e1a344164987609ad078b449e261e51
qwertyAL <sergeyiksanov25@gmail.com>
2024-05-31 21:33:19 +0300

    add ProfileScreen

--- app/src/main/java/com/example/promath/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/promath/ui/screen/ProfileScreen.kt
@@ -1,1 +1,80 @@
 package com.example.promath.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.promath.ui.themenew.palette
+
+@Composable
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = palette.base100)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        ProfileCard()
+    }
+}
+
+@Composable
+private fun ProfileCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = palette.base200, shape = RoundedCornerShape(10.dp))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Profile", color = palette.baseContent, fontWeight = FontWeight.SemiBold, fontSize = 20.sp)
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = null,
+            modifier = Modifier
+                .height(80.dp)
+                .width(80.dp),
+            tint = palette.baseContent
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Username",
+                color = palette.baseContent,
+                fontSize = 16.sp
+            )
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Create, contentDescription = null, tint = palette.baseContent)
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewProfileScreen() {
+    ProfileScreen()
+}
