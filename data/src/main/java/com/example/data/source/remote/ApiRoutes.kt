7af43a61adc7bcb63ce10a2f36e94312e70946e0
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-04 13:10:02 +0300

    add ApiRoutes

---
+++ data/src/main/java/com/example/data/source/remote/ApiRoutes.kt
@@ -0,0 +1,9 @@
+package com.example.data.source.remote
+
+object ApiRoutes {
+
+    private const val BASE_URL = ""
+    const val USERS = "$BASE_URL/users/"
+    const val GAME_SESSIONS = "$BASE_URL/game_sessions/"
+
+}
