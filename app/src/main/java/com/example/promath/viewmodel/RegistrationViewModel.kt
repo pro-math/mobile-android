d43d807de438288598a5560531ed1ac64d10d4d6
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:41:18 +0300

    change RegistrationViewModel

--- app/src/main/java/com/example/promath/viewmodel/RegistrationViewModel.kt
+++ app/src/main/java/com/example/promath/viewmodel/RegistrationViewModel.kt
@@ -3,9 +3,11 @@ package com.example.promath.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.models.ResultModel
 import com.example.domain.usecase.RegistrationUserUseCase
 import com.example.domain.usecase.SetTokenToLocalStorageUseCase
+import kotlinx.coroutines.launch
 
 class RegistrationViewModel(
     private val registrationUserUseCase: RegistrationUserUseCase,
@@ -19,12 +21,14 @@ class RegistrationViewModel(
         login: String,
         password: String
     ) {
-        val result = registrationUserUseCase.registration(login = login, password = password)
+        viewModelScope.launch {
+            val result = registrationUserUseCase.registration(login = login, password = password)
 
-        _registrationResult.postValue(result)
+            _registrationResult.postValue(result)
 
-        if(result.status == ResultModel.Status.SUCCESS) {
-            setTokenToLocalStorageUseCase.set(token = result.data!!)
+            if (result.status == ResultModel.Status.SUCCESS) {
+                setTokenToLocalStorageUseCase.set(result.data!!)
+            }
         }
     }
 
