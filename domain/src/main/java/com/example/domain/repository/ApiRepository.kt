c399cc722229618085286af192fd5aa15498bf59
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:30:46 +0300

    replace Result<String> to ResultModel<String> in loginUser in ApiRepository

--- domain/src/main/java/com/example/domain/repository/ApiRepository.kt
+++ domain/src/main/java/com/example/domain/repository/ApiRepository.kt
@@ -1,11 +1,15 @@
 package com.example.domain.repository
 
+import com.example.domain.models.GameSession
+import com.example.domain.models.ResultModel
+import com.example.domain.models.UserModel
+
 interface ApiRepository {
 
     suspend fun loginUser(
         login: String,
         password: String
-    ): Result<String>
+    ): ResultModel<String>
 
     suspend fun registrationUser(
         login: String,
