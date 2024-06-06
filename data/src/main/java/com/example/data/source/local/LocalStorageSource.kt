d40a2e4dee4cc423b28598151de9225953ed4ffe
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 12:26:16 +0300

    add getCurrentTheme in LocalStorageSource

--- data/src/main/java/com/example/data/source/local/LocalStorageSource.kt
+++ data/src/main/java/com/example/data/source/local/LocalStorageSource.kt
@@ -20,4 +20,13 @@ class LocalStorageSource(
         context.getSharedPreferences("authToken", Context.MODE_PRIVATE).edit().putString("token", token).apply()
     }
 
+    fun getCurrentTheme(): ResultModel<String> {
+        val theme: String = context.getSharedPreferences("theme", Context.MODE_PRIVATE).getString("theme", "dark").toString()
+        return if (theme != "" && theme != "null") {
+            ResultModel.success(theme)
+        } else {
+            ResultModel.failure(theme)
+        }
+    }
+
 }
