8ae17a294a4cc270b4bd74344f3aacd424839d3f
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:33:30 +0300

    add USER_ME, USER_REGISTRATION, USER_LOGIN routes in ApiRoutes

--- data/src/main/java/com/example/data/source/remote/ApiRoutes.kt
+++ data/src/main/java/com/example/data/source/remote/ApiRoutes.kt
@@ -2,8 +2,11 @@ package com.example.data.source.remote
 
 object ApiRoutes {
 
-    private const val BASE_URL = ""
-    const val USERS = "$BASE_URL/users/"
+    private const val BASE_URL = "https://4368-83-171-69-39.ngrok-free.app/api/v1"
+    const val USERS = "$BASE_URL/users"
+    const val USER_ME = "$USERS/me"
+    const val USER_REGISTRATION = "$USERS/register"
+    const val USER_LOGIN = "$USERS/token"
     const val GAME_SESSIONS = "$BASE_URL/game_sessions/"
 
 }
