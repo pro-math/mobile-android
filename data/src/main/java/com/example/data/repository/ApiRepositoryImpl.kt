6d2721ef0591398ee5c4da1f2872aa605feeaef5
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:32:19 +0300

    replace return type value in loginUser and registrationUser in ApiRepositoryImpl

--- data/src/main/java/com/example/data/repository/ApiRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/ApiRepositoryImpl.kt
@@ -1,17 +1,21 @@
 package com.example.data.repository
 
+import com.example.data.models.LoginModel
 import com.example.data.source.remote.ApiRemoteSource
+import com.example.domain.models.GameSession
+import com.example.domain.models.ResultModel
+import com.example.domain.models.UserModel
 import com.example.domain.repository.ApiRepository
 
 class ApiRepositoryImpl(
     private val apiRemoteSource: ApiRemoteSource
 ): ApiRepository {
 
-    override suspend fun loginUser(login: String, password: String): Result<String> {
+    override suspend fun loginUser(login: String, password: String): ResultModel<String> {
         return apiRemoteSource.loginUser(login = login, password = password)
     }
 
-    override suspend fun registrationUser(login: String, password: String): Result<String> {
+    override suspend fun registrationUser(login: String, password: String): ResultModel<String> {
         return apiRemoteSource.registrationUser(login = login, password = password)
     }
 
