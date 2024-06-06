80e7538f5a15dbd959e645567796fc45a64387bb
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:38:51 +0300

    add di to domainModule

--- app/src/main/java/com/example/promath/di/DomainModule.kt
+++ app/src/main/java/com/example/promath/di/DomainModule.kt
@@ -1,8 +1,11 @@
 package com.example.promath.di
 
+import com.example.data.repository.ApiRepositoryImpl
 import com.example.data.repository.LocalStorageRepositoryImpl
+import com.example.domain.usecase.CreateGameSessionUseCase
 import com.example.domain.usecase.GenerateExampleUseCase
 import com.example.domain.usecase.GetTokenFromLocalStorageUseCase
+import com.example.domain.usecase.GetUserUseCase
 import com.example.domain.usecase.LoginUserUseCase
 import com.example.domain.usecase.RegistrationUserUseCase
 import com.example.domain.usecase.SetTokenToLocalStorageUseCase
@@ -14,10 +17,14 @@ val domainModule = module {
 
     factory<GetTokenFromLocalStorageUseCase> { GetTokenFromLocalStorageUseCase(localStorageRepository = get<LocalStorageRepositoryImpl>()) }
 
-    factory<LoginUserUseCase> { LoginUserUseCase(apiRepository = get()) }
+    factory<LoginUserUseCase> { LoginUserUseCase(apiRepository = get<ApiRepositoryImpl>()) }
 
-    factory<RegistrationUserUseCase> { RegistrationUserUseCase(apiRepository = get()) }
+    factory<RegistrationUserUseCase> { RegistrationUserUseCase(apiRepository = get<ApiRepositoryImpl>()) }
 
     factory<SetTokenToLocalStorageUseCase> { SetTokenToLocalStorageUseCase(localStorageRepository = get<LocalStorageRepositoryImpl>()) }
+
+    factory<GetUserUseCase> { GetUserUseCase(apiRepository = get<ApiRepositoryImpl>()) }
+
+    factory<CreateGameSessionUseCase> { CreateGameSessionUseCase(apiRepository = get<ApiRepositoryImpl>()) }
 
 }
