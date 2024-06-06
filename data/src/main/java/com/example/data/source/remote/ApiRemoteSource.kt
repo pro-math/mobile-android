d775c86eff9580cd669b8fe5b85abca8e5161c96
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:28:27 +0300

    add realization loginUser in ApiRemoteSource

--- data/src/main/java/com/example/data/source/remote/ApiRemoteSource.kt
+++ data/src/main/java/com/example/data/source/remote/ApiRemoteSource.kt
@@ -5,8 +5,24 @@ class ApiRemoteSource {
     suspend fun loginUser(
         login: String,
         password: String
-    ): Result<String> {
-        TODO("LOGIN USER")
+    ): ResultModel<String> {
+        try {
+            val response = httpClient.post(ApiRoutes.USER_LOGIN) {
+                contentType(ContentType.Application.Json)
+                setBody("""{"username":"$login", "password":"$password"}""")
+            }
+            if (response.status.value in 200..299) {
+                val loginModel: LoginModel = response.body()
+                return ResultModel.success(loginModel.access_token)
+            } else if (response.status.value in 400..499) {
+                val detailModel: DetailModel = response.body()
+                return ResultModel.failure(message = detailModel.detail)
+            } else {
+                return ResultModel.failure(message = "Server error")
+            }
+        } catch (e: Exception) {
+            return ResultModel.failure(message = e.message)
+        }
     }
 
     suspend fun registrationUser(
