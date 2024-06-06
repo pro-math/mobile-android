adbfabcdab79126cfaa00606ffdde4fc87df5203
qwertyAL <sergeyiksanov25@gmail.com>
2024-06-06 11:40:25 +0300

    change MainViewModel

--- app/src/main/java/com/example/promath/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/promath/viewmodel/MainViewModel.kt
@@ -5,9 +5,18 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.models.GameMode
+import com.example.domain.models.GameSession
+import com.example.domain.models.ResultModel
+import com.example.domain.usecase.CreateGameSessionUseCase
+import com.example.domain.usecase.GetTokenFromLocalStorageUseCase
 import com.example.generationOfExamples.ExampleGenerator
 import com.example.generationOfExamples.ExampleModel
 import com.example.generationOfExamples.TypeOperation
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
 // писька
 
 class MainViewModel(
@@ -25,7 +34,7 @@ class MainViewModel(
     val countExamples: LiveData<Int> = _countExamples
 
     val currentDifficulty: MutableLiveData<Int> = MutableLiveData(0)
-    val currentOperation: MutableLiveData<Int> = MutableLiveData(0)
+    val currentOperation: MutableLiveData<MutableList<Boolean>> = MutableLiveData(mutableListOf(true, true, true, true))
     val currentType: MutableLiveData<Int> = MutableLiveData(0)
 
     val currentTime: MutableLiveData<Int> = MutableLiveData(0)
@@ -47,6 +56,7 @@ class MainViewModel(
         }
     }
 
+    private val examplesList: MutableList<com.example.domain.models.ExampleModel> = mutableListOf()
 
     fun startTimer() {
         val s = when (currentTime.value) {
@@ -77,19 +87,87 @@ class MainViewModel(
         _countSuccessAnswer.postValue(_countSuccessAnswer.value!! + 1)
     }
 
-    fun addCountExample() {
+    fun addCountExample(exampleModel: ExampleModel, userAns: String) {
         _countExamples.postValue(_countExamples.value!! + 1)
+        examplesList.add(com.example.domain.models.ExampleModel(
+            typeOperation = when (exampleModel.typeOperation) {
+                TypeOperation.PLUS -> "+"
+                TypeOperation.MINUS -> "-"
+                TypeOperation.MULTIPLY -> "*"
+                TypeOperation.DIVIDE -> "/"
+            },
+            number1 = exampleModel.number1,
+            number2 = exampleModel.number2,
+            correctAnswer = exampleModel.answer,
+            answer = try {
+                userAns.toInt()
+            } catch (e: Exception) {
+                -1
+            }
+        ))
+    }
+
+    fun createSession() {
+        val token = getTokenFromLocalStorageUseCase.execute()
+
+        if (token.status == ResultModel.Status.SUCCESS) {
+            viewModelScope.launch {
+                createGameSessionUseCase.create(
+                    gameSession = GameSession(
+                        gameMode = if (currentType.value == 0) {
+                            GameMode.TimeMode
+                        } else {
+                            GameMode.CountMode
+                        },
+                        duration = 0,
+                        mathOperations = buildList {
+                            if (currentOperation.value!![0]) {
+                                add("+")
+                            }
+                            if (currentOperation.value!![1]) {
+                                add("-")
+                            }
+                            if (currentOperation.value!![2]) {
+                                add("*")
+                            }
+                            if (currentOperation.value!![3]) {
+                                add("/")
+                            }
+                        },
+                        examplesCategory = if (currentDifficulty.value == 0) {
+                            10
+                        } else if (currentDifficulty.value == 1) {
+                            100
+                        } else {
+                            1000
+                        },
+                        examples = examplesList,
+                        totalCount = _countExamples.value!!,
+                        correctCount = _countSuccessAnswer.value!!
+                    ),
+                    token = token.data!!
+                )
+            }
+        }
     }
 
     fun loadExample() {
-        _example.postValue(exampleGenerator.generateExample(
-            typeOperation = when (currentOperation.value) {
-                0 -> TypeOperation.PLUS
-                1 -> TypeOperation.MINUS
-                2 -> TypeOperation.MULTIPLY
-                3 -> TypeOperation.DIVIDE
-                else -> TypeOperation.PLUS
-            },
+        val operationsList: List<TypeOperation> = buildList {
+            if (currentOperation.value!![0]) {
+                add(TypeOperation.PLUS)
+            }
+            if (currentOperation.value!![1]) {
+                add(TypeOperation.MINUS)
+            }
+            if (currentOperation.value!![2]) {
+                add(TypeOperation.MULTIPLY)
+            }
+            if (currentOperation.value!![3]) {
+                add(TypeOperation.DIVIDE)
+            }
+        }
+        val example = exampleGenerator.generateExample(
+            typesOperations = operationsList,
             border1 = when (currentDifficulty.value) {
                 0 -> 10
                 1 -> 100
@@ -102,7 +180,8 @@ class MainViewModel(
                 2 -> 9999
                 else -> 99
             }
-        ))
+        )
+        _example.postValue(example)
 //        _countExamples.postValue(_countExamples.value!! + 1)
     }
 
